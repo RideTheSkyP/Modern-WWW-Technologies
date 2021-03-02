844b12a786477d1049f47d457a63ab6cc9dea645
Push <49253201+RideTheSkyP@users.noreply.github.com>
2021-03-03 00:45:30 +0100

    Added navigation drawer & moved to different files.

---
+++ L1/js/index.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "25%" //opens side navbar by 70 percent
+    document.getElementById("backdrop").style.display = "block" //displays overlay
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0"
+    document.getElementById("backdrop").style.display = "none"
+}
+
