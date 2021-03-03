b4680ae64021304297d69bd8149baa0a4b1bd401
Push <49253201+RideTheSkyP@users.noreply.github.com>
2021-03-03 23:00:15 +0100

    Added sidebar and some info.

--- L1/js/index.js
+++ L1/js/index.js
@@ -1,10 +1,9 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "25%" //opens side navbar by 70 percent
-    document.getElementById("backdrop").style.display = "block" //displays overlay
+    document.getElementById("mySidenav").style.width = "25%"
+    document.getElementById("backdrop").style.display = "block"
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0"
     document.getElementById("backdrop").style.display = "none"
 }
-
