fd4fd4ff5ec7146e2a8cab38f96ce321ad2fd7ba
Push <49253201+RideTheSkyP@users.noreply.github.com>
2021-06-03 13:09:10 +0200

    L4Ex2Ch2

--- L4/Ex2/lib/main.dart
+++ L4/Ex2/lib/main.dart
@@ -19,6 +19,18 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class OrientationSwitcher extends StatelessWidget {
+  final List<Widget> children;
+
+  const OrientationSwitcher({Key? key, required this.children}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    bool isScreenWide = MediaQuery.of(context).size.width >= 720;
+    return isScreenWide ? Column(children: children) : Row(children: children);
+  }
+}
+
 class MyHomePage extends StatefulWidget {
   // MyHomePage({required Key key}) : super(key: key);
 
@@ -26,13 +38,15 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+{
   TextEditingController _laTeXInputController = TextEditingController(
-      text: r"$${\rm{A function: } f(x) = \frac{5}{3} \cdot x}$$");
+      text: r"$${f(x) = \frac{5}{3} \cdot x}$$");
   late String _laTeX;
 
   @override
-  void initState() {
+  void initState()
+  {
     _renderLaTeX();
     super.initState();
   }
@@ -40,41 +54,53 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("List4"),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Center(
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    onChanged: (text)
-                    {
-                      print("First text field: $text");
-                      _renderLaTeX();
-                    },
-                    keyboardType: TextInputType.multiline,
-                    maxLines: null,
-                    decoration: InputDecoration(
+      appBar: AppBar(
+        title: Text("List4"),
+      ),
+      body: Container(
+        child: new OrientationSwitcher(
+          children: <Widget>[
+            new Expanded (
+              flex: 4,
+              child : Column(
+                children: <Widget>[
+                  new Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      onChanged: (text)
+                      {
+                        print("First text field: $text");
+                        _renderLaTeX();
+                      },
+                      keyboardType: TextInputType.multiline,
+                      maxLines: null,
+                      decoration: InputDecoration(
                         hintText: "Enter term",
+                      ),
+                      controller: _laTeXInputController,
                     ),
-                    controller: _laTeXInputController,
                   ),
-                ),
-                Container(
+                ],
+              ),
+            ),
+            new Expanded(
+              flex: 6,
+              child: Column(
+                children: <Widget>[
+                  new Padding(
+                    padding: const EdgeInsets.all(8.0),
                     child: Builder(
                       builder: (context) => KaTeX(
                         laTeXCode: Text(_laTeX,
                             style: Theme.of(context).textTheme.bodyText2),
                       ),
-                    ))
-              ],
-            ),
-          ),
+                    )
+                  ),
+                ],
+              ),)
+          ],
         ),
+      )
     );
   }
 
